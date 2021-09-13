80117d3ee79146569930646fec8a85fd03cd96eb
ecaterinagherbovei <catea.gherbovei@gmail.com>
2021-09-13 12:27:23 +0300

    correcting files loading faults

--- app/src/main/java/com/example/filemanager/AllFilesFragment.kt
+++ app/src/main/java/com/example/filemanager/AllFilesFragment.kt
@@ -8,12 +8,10 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -28,16 +26,11 @@ class AllFilesFragment : Fragment(), ItemClickListener {
     private lateinit var binding: FragmentAllFilesBinding
     private lateinit var viewModel: AllFilesViewModel
 
-    companion object {
-        const val PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 333
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this, AllFilesViewModelFactory(applicationContext()))
             .get(AllFilesViewModel::class.java)
         adapter = ListAdapter(this)
-        fetchFiles()
     }
 
     override fun onCreateView(
@@ -67,13 +60,18 @@ class AllFilesFragment : Fragment(), ItemClickListener {
         })
     }
 
+    override fun onStart() {
+        super.onStart()
+        fetchFiles()
+    }
+
     override fun onItemClickListener() {
         Toast.makeText(context, "Item clicked", Toast.LENGTH_SHORT).show()
     }
 
     private fun fetchFiles() {
         if (!checkPermission()) {
-            showPermissionDialog();
+            showPermissionDialog()
         } else {
             viewModel.fetchFiles()
         }
@@ -81,23 +79,16 @@ class AllFilesFragment : Fragment(), ItemClickListener {
 
     private fun showPermissionDialog() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            try {
-                val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
-                intent.addCategory("android.intent.category.DEFAULT")
-                intent.data = Uri.parse(String.format("package:%s", arrayOf<Any>(applicationContext().packageName)))
-                activity?.startActivityForResult(intent, 2000)
-            } catch (e: Exception) {
-                val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
-                activity?.startActivityForResult(intent, 2000)
-            }
-        } else {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
-                PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE
+            val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+            startActivity(
+                Intent(
+                    Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri
+                )
             )
-            Log.d("RequestPermission", "Requesting permission")
-        }
+        } else requestPermissions(
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE
+        )
     }
 
     private fun checkPermission(): Boolean {
@@ -108,24 +99,16 @@ class AllFilesFragment : Fragment(), ItemClickListener {
                 applicationContext(),
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
             )
-            val read = ContextCompat.checkSelfPermission(
-                applicationContext(),
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-            write == PackageManager.PERMISSION_GRANTED &&
-                    read == PackageManager.PERMISSION_GRANTED
+            write == PackageManager.PERMISSION_GRANTED
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        Log.d("OnPermissionResult", "on permission result is here")
         if (requestCode == PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE) {
             if (grantResults.isNotEmpty()) {
                 val write = grantResults[0] == PackageManager.PERMISSION_GRANTED
-                val read = grantResults[1] == PackageManager.PERMISSION_GRANTED
-                if (read && write) {
-                    Toast.makeText(context, "Permission allowed", Toast.LENGTH_SHORT).show()
+                if (write) {
+                    fetchFiles()
                 } else {
                     Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
@@ -133,15 +116,7 @@ class AllFilesFragment : Fragment(), ItemClickListener {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 2000) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                if (Environment.isExternalStorageManager()) {
-                    Toast.makeText(context, "Permission allowed", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+    companion object {
+        const val PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 333
     }
 }
